0970359fcf3d3c69ab4e50f3f7327df3353e4dbd
Ricardo Antonio Gomez Villalobos <bardack134@gmail.com>
2023-12-02 07:38:50 +0900

    tratando de importar el proyecto a render

--- CRUD_hospital/settings.py
+++ CRUD_hospital/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",  #newly added
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -148,11 +149,12 @@ LOGIN_URL = '/autenticacion/login/'
 
 import os
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Esta es una ruta de ejemplo, puedes tener más directorios aquí.
-]
-
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),  # Esta es una ruta de ejemplo, puedes tener más directorios aquí.
+# ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 
