1d3d5624eb535b1c837210315b55eac9f91ec0ea
Ricardo Antonio Gomez Villalobos <142977989+bardack134@users.noreply.github.com>
2024-06-13 06:54:59 +0900

    Update urls.py

--- CRUD/urls.py
+++ CRUD/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 #de las vistas importamos las clases que creamos
+# views から作成したクラスをインポートします
 from .views import RegisterPatient, RegisterPatientSP, RegisterPatientJP
 
 
 urlpatterns = [
     # Asignamos la vista RegisterView a la URL raiz, con el nombre 'autenticacion'.
-    
+    # ルートURLにviewに書いてあるクラスを設定します
     path('', RegisterPatient.as_view(), name='RegisterPatient'),
     path('sp/', RegisterPatientSP.as_view(), name='RegistroPaciente'),
     path('jp/', RegisterPatientJP.as_view(), name='RegistroPacienteJP'),
