bbf02f54d3348a3fab086e54918bd8fa63de88f9
Ricardo Antonio Gomez Villalobos <bardack134@gmail.com>
2024-09-22 17:18:29 +0900

    build.sh

--- CRUD_hospital/build.sh
+++ CRUD_hospital/build.sh
@@ -5,3 +5,25 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+
+
+# Crea el superusuario
+python -c "
+import os
+import django
+from django.contrib.auth import get_user_model
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CRUD_hospital.settings')
+django.setup()
+
+User = get_user_model()
+username = 'admin'  
+email = 'admin@gmail.com'  
+password = 'admin'  
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f'Superuser {username} created.')
+else:
+    print(f'Superuser {username} already exists.')
+"
